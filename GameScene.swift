0ed1c174311dd2368fcb3320d6ccb283cbf6bfaa
GabeLops <gabrieldlops@gmail.com>
2020-01-01 13:29:59 -0800

    Penguin Game

---
+++ GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  project14
+//
+//  Created by Gabriel Lops on 1/1/20.
+//  Copyright © 2020 Gabriel Lops. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+}
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+}
+    
+    
+    
