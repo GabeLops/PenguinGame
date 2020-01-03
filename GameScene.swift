d0cfe3f18484926f341f4adf7d00cb274fc23fcc
GabeLops <gabrieldlops@gmail.com>
2020-01-02 23:49:17 -0800

    fix game scene plus effects

--- GameScene.swift
+++ GameScene.swift
@@ -9,12 +9,14 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
     override func didMove(to view: SKView) {
         
 }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         <#code#>
+}
 }
     
     
