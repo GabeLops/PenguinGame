0304c3c4568e7753320bb9e3451454b21443b4c8
GabeLops <gabrieldlops@gmail.com>
2020-01-25 18:01:55 -0800

    FIX BUG WITH WHACKAMOLE NOT WHACKING CORRECTLY

--- WhackSlot.swift
+++ WhackSlot.swift
@@ -11,8 +11,6 @@ import UIKit
 
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
-    var mudParticles: SKEmitterNode!
-    var smokeEmitter: SKEmitterNode!
     
     var isVisible = false
     var isHit = false
@@ -27,45 +25,42 @@ class WhackSlot: SKNode {
         cropNode.position = CGPoint(x: 0, y: 15)
         cropNode.zPosition = 1
         cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
         charNode = SKSpriteNode(imageNamed: "penguinGood")
-        charNode.position = CGPoint(x: 0 , y: -90)
+        charNode.position = CGPoint(x: 0, y: -90)
         charNode.name = "character"
         cropNode.addChild(charNode)
         
         addChild(cropNode)
     }
+    
     func show(hideTime: Double) {
-        if isVisible {return}
+        if isVisible { return }
         
         charNode.xScale = 1
         charNode.yScale = 1
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         
-        charNode.run(SKAction.moveBy(x:0, y: 80, duration: 0.05))
         isVisible = true
-        isHit = true
-        
-        mudParticles = SKEmitterNode(fileNamed: "mudEmitter.sks")
-        mudParticles?.position = charNode.position
-        mudParticles.run(SKAction.moveBy(x: 0, y: 70, duration: 0.05))
-        addChild(mudParticles!)
+        isHit = false
         
         if Int.random(in: 0...2) == 0 {
             charNode.texture = SKTexture(imageNamed: "penguinGood")
             charNode.name = "charFriend"
-        }else {
+        } else {
             charNode.texture = SKTexture(imageNamed: "penguinEvil")
             charNode.name = "charEnemy"
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
-            [weak self] in
-            self?.hide()
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [unowned self] in
+            self.hide()
         }
     }
     
     func hide() {
-        if !isVisible {return}
-        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y:-80, duration:0.05))
         isVisible = false
     }
     
@@ -73,16 +68,9 @@ class WhackSlot: SKNode {
         isHit = true
         
         let delay = SKAction.wait(forDuration: 0.25)
-        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
-        let notVisible = SKAction.run {
-            [weak self] in self?.isVisible = false
-        }
-        smokeEmitter = SKEmitterNode(fileNamed: "smokeEmitter.sks")
-        smokeEmitter?.position = charNode.position
-        smokeEmitter.zPosition = 1
-        addChild(smokeEmitter)
-        let sequence = SKAction.sequence([delay,hide,notVisible])
-        charNode.run(sequence)
+        let hide = SKAction.moveBy(x: 0, y:-80, duration:0.5)
+        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
+        charNode.run(SKAction.sequence([delay, hide, notVisible]))
     }
-
+    
 }
